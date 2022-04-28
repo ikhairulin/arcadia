efd50726a8bc55ff66225dbb0aa2fa1c7d03e8ca
ikhairulin <ikhairulin@gmail.com>
2022-04-28 21:31:18 +0300

    добавил комменты

--- Parser_DA_img.py
+++ Parser_DA_img.py
@@ -9,7 +9,7 @@ start_time = d.datetime.now()
 alpha_dir = "D:\Pictures"
 header={'User-Agent':"Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/43.0.2357.134 Safari/537.36"}
 
-
+# не работающий блок авторизации
 datas = {
     'uгsername': 'Zebul',
     'password': 'sNn@jNQ7Mc4cb7L'

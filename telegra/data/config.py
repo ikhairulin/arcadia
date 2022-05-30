4a0b37af005145442b5bfa3b55bd229b1f089ebc
ikhairulin <104314805+ikhairulin@users.noreply.github.com>
2022-05-30 23:38:04 +0300

    Add files via upload
    add config.py

---
+++ telegra/data/config.py
@@ -0,0 +1,12 @@
+
+
+# a parent directory for temporary files
+alpha_dir = 'D:\\'
+
+
+# enter your token here
+my_token = ''
+
+#  need to specify the path to the file UnRAR.exe in WinRar dir
+
+UnRAR_path = r"C:\Program Files\WinRAR\UnRAR.exe"
